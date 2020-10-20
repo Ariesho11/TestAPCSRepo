4d6ca481bb9f9a131133b2c983dbd2fc45c4a937
Aries <aho11@stuy.edu>
2020-10-20 07:15:27 -0400

    I tested this program and i works now

---
+++ Demo_.java
@@ -0,0 +1,17 @@
+public class Demo_{
+  public static String dem(int a){
+    int print = 1;
+    String ns = "";
+    for (int i = 0; i<=a-1; i++){
+      for (int j = 1; j <= a-i; j++){
+        ns = ns + String.valueOf(print);
+      }
+      print = print + 1;
+      ns = ns + "\n";
+    }
+    return ns;
+  }
+  public static void main(String[] args){
+    System.out.print(Demo_.dem(5));
+  }
+}
